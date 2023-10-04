abc3d0fa8e143bd62e88dc19a9e304b8bb0f23b0
aevik <aevik@JABI>
2023-10-04 12:27:18 -0400

    fix: issue notion

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,51 @@
+import { Client } from "@notionhq/client";
 
+const notionSecret = process.env.NOTION_SECRET;
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+const notion = new Client({ auth: notionSecret });
 
 const fetchFromNotion = async () => {
-	const res = await fetch('http://localhost:3000/api/notion')
-	const data = await res.json()
-	return JSON.parse(data)
-}
+  const query = await notion.databases.query({
+    database_id: notionDatabaseId || "",
+  });
+
+  const { results } = query;
+
+  return results || [];
+};
 
 export default async function Home() {
+  const rows = await fetchFromNotion();
+  console.log("rows on front end", rows);
 
-  const rows: rowsStructured = await fetchFromNotion()
-  console.log('rows on front endffvvv-------------------', rows)
+  const [columns] = rows;
+  const headers = Object.keys(columns);
 
   return (
-    <>sssvvv</>
-  )
+    <table className="min-w-full border border-gray-300 divide-y divide-gray-300">
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th
+              className="px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider"
+              key={header}
+            >
+              {header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-300">
+        {rows.map((row: any) => (
+          <tr key={row.id}>
+            {headers.map((header, j) => (
+              <td className="px-6 py-4 whitespace-no-wrap" key={j}>
+                {row[header]?.toString()}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
--- pages/api/notion.ts
+++ pages/api/notion.ts
@@ -1,34 +1,37 @@
-import { Client } from '@notionhq/client'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { Client } from "@notionhq/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const notionSecret = process.env.NOTION_SECRET
-const notionDatabaseId = process.env.NOTION_DATABASE_ID
+const notionSecret = process.env.NOTION_SECRET;
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
 
-const notion = new Client({ auth: notionSecret })
+const notion = new Client({ auth: notionSecret });
 
 type Row = {
-	first_name: { id: string, rich_text: {text: { content: string } }[]}
-	link_to_youtube: { id: string, url: string }
-	name: { id: string, title: { text: { content: string } }[] }
+  first_name: { id: string; rich_text: { text: { content: string } }[] };
+  link_to_youtube: { id: string; url: string };
+  name: { id: string; title: { text: { content: string } }[] };
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (!notionSecret || !notionDatabaseId)
+    throw new Error("Missing notion secret or DB-ID.");
+
+  const query = await notion.databases.query({
+    database_id: notionDatabaseId,
+  });
+
+  // @ts-ignore
+  const rows = query.results.map((res) => res.properties) as Row[];
+  // console.log(query)
+
+  const rowsStructured: rowsStructured = rows.map((row) => ({
+    name: row.name.title[0].text.content,
+    first_name: row.first_name.rich_text[0].text.content,
+    url: row.link_to_youtube.url,
+  }));
+
+  res.status(200).json(JSON.stringify(rowsStructured));
 }
-
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
-
-	if(!notionSecret || !notionDatabaseId) throw new Error('Missing notion secret or DB-ID.')
-
-	const query = await notion.databases.query({
-		database_id: notionDatabaseId,
-	})
-
-	// @ts-ignore
-	const rows = query.results.map((res) => res.properties) as Row[]
-	// console.log(query)
-
-	const rowsStructured: rowsStructured = rows.map((row) =>({
-		name: row.name.title[0].text.content,
-		first_name: row.first_name.rich_text[0].text.content,
-		url: row.link_to_youtube.url
-	}))
-
-	res.status(200).json(JSON.stringify(rowsStructured))	
-}  
